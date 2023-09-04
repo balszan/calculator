a6f5c5daccb05d9f6833c29afeadf7f1988538de
balszan <kbalszan@gmail.com>
2023-09-04 15:45:33 +0100

    Add functionality

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
 let result = document.querySelector('#result');
 
+const plusButton = document.querySelector('#plus');
+const minusButton = document.querySelector('#minus');
+const timesButton = document.querySelector('#times');
+const divideButton = document.querySelector('#divide');
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear');
+const backButton = document.querySelector('#back');
+const dotButton = document.querySelector('#dot');
+
+const oneButton = document.querySelector('#one');
+const twoButton = document.querySelector('#two');
+const threeButton = document.querySelector('#three');
+const fourButton = document.querySelector('#four');
+const fiveButton = document.querySelector('#five');
+const sixButton = document.querySelector('#six');
+const sevenButton = document.querySelector('#seven');
+const eightButton = document.querySelector('#eight');
+const nineButton = document.querySelector('#nine');
+
+let firstNum ="";
+let operand = "";
+let secondNum = "";
+
+function operate(first,operand,second) {
+    
+}
+
 function displayResult(res) {
     result.textContent = res;
 }
@@ -27,3 +54,4 @@ function divide(...numbers) {
     let total = numbers.reduce((acc,curr) => acc = acc/curr);
     return total;
 }
+
